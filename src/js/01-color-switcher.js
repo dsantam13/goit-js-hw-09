d855540b9da9c752ea3d8890cdcc3f51e48ecda9
Daniel Santa Mateus <dsm50561@hotmail.com>
2023-06-18 17:11:29 -0500

    first task - Homework 9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,26 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
+
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const bodyElement = document.querySelector("body");
+let timerId = null;
+btnStop.disabled = true;
+
+function changeColor() {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+  timerId = setInterval(() => {
+    bodyElement.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+function stopChangeColor() {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  clearInterval(timerId);
+};
+
+btnStart.addEventListener("click", changeColor);
+btnStop.addEventListener("click", stopChangeColor);
