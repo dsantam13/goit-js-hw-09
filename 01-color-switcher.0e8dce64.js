2c9df1fe5ad2d0a3d2938f845ba419c27e7f9879
dsantam13 <63376231+dsantam13@users.noreply.github.com>
2023-06-18 22:11:48 +0000

    Deploying to gh-pages from @ dsantam13/goit-js-hw-09@d855540b9da9c752ea3d8890cdcc3f51e48ecda9 🚀

---
+++ 01-color-switcher.0e8dce64.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.querySelector("body"),d=null;e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(d)}))}();
+//# sourceMappingURL=01-color-switcher.0e8dce64.js.map
