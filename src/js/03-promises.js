b2f619bf598623781bde92c7dd678cb07a43a1b4
Daniel Santa Mateus <dsm50561@hotmail.com>
2023-06-19 22:58:28 -0500

    Third task - Homework 9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const firstDelay = document.querySelector('input[name="delay"]');
+  const delayStep = document.querySelector('input[name="step"]');
+  const amountInput = document.querySelector('input[name="amount"]');
+
+  const firstDelayNum = parseInt(firstDelay.value);
+  const delayStepNum = parseInt(delayStep.value);
+  const amountInputNum = parseInt(amountInput.value);
+
+  let currentDelay = firstDelayNum;
+
+  for (let i = 0; i < amountInputNum; i++) {
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    currentDelay += delayStepNum;
+  }
+});
